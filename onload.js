de76a1bb94e0b56b1d96b1546570fcbebe835816
Jay Deuskar <coffeejay@me.com>
2015-07-20 00:04:32 -0400

    swag

--- onload.js
+++ onload.js
@@ -1,14 +1,33 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 window.onload = function() {
   $("#login").click(function(event) {
-    console.log('test')
     chrome.identity.launchWebAuthFlow(
-    {'url': 'http://localhost:3000/oauth/authorize?client_id=1e860423b3d54b09101c9fd7c1a225fa14b32dd2a0b7da56f10914c9e5c044f1&redirect_uri=https%3A%2F%2Fljcjhaejllpbeiamjnbldcngmjjoihln.chromiumapp.org%2Fvyrtex-chrome-extension&response_type=code', 'interactive': true},
+    {'url': 'http://localhost:3000/oauth/authorize?client_id=7b529aed5e89c14bd45c4cb949f220e5bc4cc440003dd7604d3fc6ed43f29844&redirect_uri=https%3A%2F%2Fhnjjholodahaklljjenjpfbkkghppdpk.chromiumapp.org%2Fvyrtex-chrome-extension&response_type=code', 'interactive': true},
     function(redirect_url) { 
-      console.log(redirect_url) 
+       console.log(redirect_url);
+       var code = redirect_url.substring(redirect_url.indexOf("=") + 1, redirect_url.length );
+       exchangeCodeForToken(code);
     });
   });
+
+  function exchangeCodeForToken(code) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST',
+             'http://localhost:3000/oauth/token?' +
+             'client_id=' + "7b529aed5e89c14bd45c4cb949f220e5bc4cc440003dd7604d3fc6ed43f29844" +
+             '&grant_type=' + "authorization_code" +
+             '&redirect_uri=' + "https://hnjjholodahaklljjenjpfbkkghppdpk.chromiumapp.org/vyrtex-chrome-extension" +
+             '&code=' + code);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa('7b529aed5e89c14bd45c4cb949f220e5bc4cc440003dd7604d3fc6ed43f29844' + ':' + '48ec91d8b544bc13f6ddc2528a64105da385cbde6b4345972026eb3eaf8abd0a'));
+    xhr.onload = function () {
+      if (this.status === 200) {
+        var response = JSON.parse('"'+this.responseText+'"');
+        response = response.substring(0,response.indexOf('&'));
+        access_token = response;
+        console.log(access_token);
+      }
+    };
+    xhr.send();
+  }
 }
