1d8192e6fbdcd69b17c7db9709f360d08fdaef85
Chintan Parikh <chintan@pennywhale.com>
2015-07-20 01:31:13 -0400

    Basic functionality

---
+++ quick_add.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+  chrome.storage.sync.get(function(object) {
+    var access_token = object.access_token;
+    
+    // Get all collections
+
+    $.ajax({
+      type: "GET",
+      url: 'http://localhost:3000/api/collections',
+      beforeSend: function(request) {
+        request.setRequestHeader('Authorization', 'Bearer ' + access_token)
+      }
+    }).done(function(response) {
+      collections = {}
+
+      for (var item in response.collections) {
+        var collection = response.collections[item];
+        collections[collection.id] = collection.name
+      };
+
+      var select = $("#collections")
+      $.each(collections, function(id, name) {
+        select.append($('<option />').val(id).text(name));
+      })
+    }).fail(function(response) {
+      console.log(response);
+    })
+  });
+}
